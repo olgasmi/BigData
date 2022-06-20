2555d43d4a8be37d000ff0f4911f7600ee75f224
olgasmi <74953268+olgasmi@users.noreply.github.com>
2022-06-20 21:08:57 +0200

    Update DataReader.scala

--- spark-maven-demo-main/spark-maven-demo-main/src/main/scala/agh/wggios/analizadanych/datareader/DataReader.scala
+++ spark-maven-demo-main/spark-maven-demo-main/src/main/scala/agh/wggios/analizadanych/datareader/DataReader.scala
@@ -2,15 +2,17 @@ package agh.wggios.analizadanych.datareader
 
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, SQLContext}
+import sbt.util.LogExchange.logger
 
 class DataReader {
 
   def read_csv(path: String, sql_context: SQLContext, schema: StructType = null, header: Boolean = false): DataFrame = {
 
     var options = sql_context.read.format("csv").option("header", header)
-    options = options.option("inferSchema", true);
+    options = options.option("inferSchema", value = true)
+    logger.info("Reading csv")
 
-    return options.load(path);
+    return options.load(path)
   }
 
 }

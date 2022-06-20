522e2ba1bdd4f344df105cefa257ebe9f786ea67
olgasmi <74953268+olgasmi@users.noreply.github.com>
2022-06-20 21:09:30 +0200

    Update DataWriter.scala

--- spark-maven-demo-main/spark-maven-demo-main/src/main/scala/agh/wggios/analizadanych/datawriter/DataWriter.scala
+++ spark-maven-demo-main/spark-maven-demo-main/src/main/scala/agh/wggios/analizadanych/datawriter/DataWriter.scala
@@ -1,10 +1,12 @@
 package agh.wggios.analizadanych.datawriter
-import org.apache.spark.sql.{DataFrame, Dataset}
+import org.apache.spark.sql.DataFrame
+import sbt.util.LogExchange.logger
 
 class DataWriter(){
 
   def write(df:DataFrame,path:String, format: String = "csv"): Unit = {
-    df.write.format(format).save(path);
+    df.write.format(format).save(path)
+    logger.info("Saving dataframe")
   }
 
 }
